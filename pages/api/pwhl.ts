6848b3d86286ab8f5736390b9a274fc722f62dfd
Kyle Johnson <kylejohnson.mn@gmail.com>
2024-10-21 19:58:19 -0500

    fix eslint errors

--- pages/api/pwhl.ts
+++ pages/api/pwhl.ts
@@ -1,13 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as ics from "ts-ics";
-const logger = require("pino")();
+import pino from "pino";
+
+const logger = pino();
 
 const everyGameId = Array.from({ length: 999 }, (_, i) => i + 1).join(",");
 const allGamesIcalUrl = `https://lscluster.hockeytech.com/components/calendar/ical_add_games.php?client_code=pwhl&game_ids=${everyGameId}`;
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<any>,
+  res: NextApiResponse<string>,
 ) {
   const teams = Array.isArray(req.query.teams)
     ? req.query.teams
