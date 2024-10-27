5f0b1832da800f76a486ea8ed2d4e3b6ef2e201f
Kyle Johnson <kylejohnson.mn@gmail.com>
2024-10-27 16:48:19 -0500

    use wordmark SVGs instead

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@
 import CodeBlock from "@/components/CodeBlock";
 
 import Image from "next/image";
-import boston from "../public/boston.png";
-import minnesota from "../public/minnesota.png";
-import montreal from "../public/montreal.png";
-import newyork from "../public/newyork.png";
-import ottawa from "../public/ottawa.png";
-import toronto from "../public/toronto.png";
+import boston from "../public/boston.svg";
+import minnesota from "../public/minnesota.svg";
+import montreal from "../public/montreal.svg";
+import newyork from "../public/newyork.svg";
+import ottawa from "../public/ottawa.svg";
+import toronto from "../public/toronto.svg";
 
 import React, { useState } from "react";
 import {
@@ -21,7 +21,7 @@ import {
 } from "@mui/material";
 
 export default function ToggleButtonsPage() {
-  const iconWidth = 100;
+  const iconWidth = 150;
   const [selectedPwhlTeams, setSelectedPwhlTeams] = useState<string[]>([
     "Minnesota",
   ]);
@@ -50,52 +50,22 @@ export default function ToggleButtonsPage() {
           >
             <Grid container spacing={2}>
               <ToggleButton value="Boston" aria-label="Boston">
-                <Stack alignItems="center">
-                  <Image src={boston} alt="Boston" width={iconWidth} />
-                  Boston
-                  <br />
-                  Fleet
-                </Stack>
+                <Image src={boston} alt="Boston" width={iconWidth} />
               </ToggleButton>
               <ToggleButton value="Minnesota" aria-label="Minnesota">
-                <Stack alignItems="center">
-                  <Image src={minnesota} alt="Minnesota" width={iconWidth} />
-                  Minnesota
-                  <br />
-                  Frost
-                </Stack>
+                <Image src={minnesota} alt="Minnesota" width={iconWidth} />
               </ToggleButton>
               <ToggleButton value="Montreal" aria-label="Montreal">
-                <Stack alignItems="center">
-                  <Image src={montreal} alt="Montreal" width={iconWidth} />
-                  Montréal
-                  <br />
-                  Victoire
-                </Stack>
+                <Image src={montreal} alt="Montreal" width={iconWidth} />
               </ToggleButton>
-              <ToggleButton value="New York" aria-label="New York">
-                <Stack alignItems="center">
-                  <Image src={newyork} alt="New York" width={iconWidth} />
-                  New York
-                  <br />
-                  Sirens
-                </Stack>
+              <ToggleButton value="New%20York" aria-label="New York">
+                <Image src={newyork} alt="New York" width={iconWidth} />
               </ToggleButton>
               <ToggleButton value="Ottawa" aria-label="Ottawa">
-                <Stack alignItems="center">
-                  <Image src={ottawa} alt="Ottawa" width={iconWidth} />
-                  Ottawa
-                  <br />
-                  Charge
-                </Stack>
+                <Image src={ottawa} alt="Ottawa" width={iconWidth} />
               </ToggleButton>
               <ToggleButton value="Toronto" aria-label="Toronto">
-                <Stack alignItems="center">
-                  <Image src={toronto} alt="Toronto" width={iconWidth} />
-                  Toronto
-                  <br />
-                  Sceptres
-                </Stack>
+                <Image src={toronto} alt="Toronto" width={iconWidth} />
               </ToggleButton>
             </Grid>
           </ToggleButtonGroup>
