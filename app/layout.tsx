f77b58254ee153c85fc1b165262661cabfad8fec
Kyle Johnson <kylejohnson.mn@gmail.com>
2025-01-04 22:17:41 -0600

    feat: add speed insights (#116)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import ThemeRegistry from "@/components/ThemeRegistry";
 import "./globals.css";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <ThemeRegistry>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ThemeRegistry>
       </body>
     </html>
