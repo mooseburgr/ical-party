fdb7b06c21993cbd88743a560825f987cb4f6a22
Kyle Johnson <kylejohnson.mn@gmail.com>
2025-06-13 16:49:25 -0500

    Update instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,14 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel({ serviceName: "ical-party" });
+  registerOTel({ 
+    serviceName: "ical-party",
+    instrumentationConfig: {
+      fetch: {
+        // By default the context is only propagated for the deployment URLs, all other URLs should be enabled explicitly in the list by string prefix or regex.
+        propagateContextUrls: [],
+      },
+    },
+    
+   });
 }
