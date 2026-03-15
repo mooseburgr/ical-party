9e7ceeaccc51d095a9e8d9fe458b5d3b6d541865
Kyle Johnson <kylejohnson.mn@gmail.com>
2026-03-15 13:04:39 -0500

    replace TKers scraper with proxy to real ical feed

--- app/api/tkers/route.ts
+++ app/api/tkers/route.ts
@@ -1,6 +1,5 @@
 import type { NextRequest } from "next/server";
-import { TEXT_CAL, USER_AGENT } from "@/app/api/pwhl/lib";
-import { generateIcalContent, getAllScheduleEvents } from "@/app/api/tkers/lib";
+import { USER_AGENT } from "@/app/api/pwhl/lib";
 
 const logger = console;
 
@@ -8,17 +7,17 @@ const logger = console;
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
 export const revalidate = 1200;
 
+const realUrl = "https://cscsports.leaguelab.com/my-schedule-feed/2395630-bd8166fcedd127268408987db277deff";
+
 export async function GET(req: NextRequest) {
-  const events = await getAllScheduleEvents();
-
-  logger.info(
-    `fetched ${events.length} TKers events for user-agent '${req.headers.get(USER_AGENT)}'`,
-  );
-
-  return new Response(generateIcalContent(events), {
+  // fetch the schedule events from the source feed
+  const resp = await fetch(realUrl, {
     headers: {
-      "content-type": TEXT_CAL,
-      "cache-control": `public, max-age=${revalidate}, must-revalidate`,
+      "user-agent": req.headers.get(USER_AGENT) ?? "",
     },
   });
+
+  logger.debug(`fetched schedule feed with status ${resp.status}`);
+
+  return resp;
 }
