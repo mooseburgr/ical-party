b1f7df803dcc4d03ff6bab70b1a80c2282c6a86d
Kyle Johnson <kylejohnson.mn@gmail.com>
2026-04-26 14:47:53 -0500

    extract & wrap client-side preview component in Suspense

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,44 +1,10 @@
-"use client";
-
-import { Box, Grid, TextField } from "@mui/material";
-import type { SchedulerEvent } from "@mui/x-scheduler/models";
-import { useQueryState } from "nuqs";
-import type React from "react";
-import { useState } from "react";
-import { fetchCalendarEvents } from "@/app/preview/lib";
-import Calendar from "@/components/Calendar";
+import { Suspense } from "react";
+import CalendarPreview from "@/app/preview/CalendarPreview";
 
 export default function CalendarPreviewPage() {
-  const [url, setUrl] = useQueryState("url", {
-    defaultValue: "",
-  });
-  const [events, setEvents] = useState<SchedulerEvent[]>();
-
-  const handleInputChange = async (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    await setUrl(event.target.value);
-    setEvents(await fetchCalendarEvents(event.target.value));
-  };
-
   return (
-    <Box sx={{ p: 4 }}>
-      <Grid container spacing={2}>
-        <Grid size={{ xs: 12 }}>
-          <TextField
-            variant="outlined"
-            fullWidth
-            label="ICS/iCal feed URL"
-            placeholder="https://example.com/calendar.ics"
-            value={url}
-            onChange={handleInputChange}
-          />
-        </Grid>
-
-        <Grid size={{ xs: 12 }}>
-          <Calendar icalUrl={url} events={events} />
-        </Grid>
-      </Grid>
-    </Box>
+    <Suspense>
+      <CalendarPreview />
+    </Suspense>
   );
 }
